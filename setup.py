20e4b4ea7ccce306d46565bd5ab75ba8150b678f
“[Anuj <[anujxgomare@gmail.com]>
2025-02-21 00:24:40 +0530

    First setup done

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages,setup
+from typing import List
+def get_requirements(file_path:str)->List[str]:  # this function will return the list of requirements
+
+    requirements=[]
+
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace('\n',"") for req in requirements]
+    
+    return requirements
+
+setup(
+name='mlproject',
+version='0.0.1',
+author='Anuj',
+author_email='anujxgomare@gmail.com',
+packages=find_packages(),
+install_requirements=get_requirements('requirements.txt')
+)
